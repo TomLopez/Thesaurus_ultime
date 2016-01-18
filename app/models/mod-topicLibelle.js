9c1746a9f42f3ac905a137c71f657906ac74b379
TomLopez <tom_lopez@natural-solutions.eu>
2016-01-18 16:39:48 +0100

    language required fields

--- app/models/mod-topicLibelle.js
+++ app/models/mod-topicLibelle.js
@@ -39,8 +39,8 @@ function(Marionette, Backbone, Config, I18n) {
         console.log('Error : ', err);
       });
         this.schema = {
-          TLib_FK_TLan_ID:      {type: 'Select', title: $.i18n.t('topicLibelle_field.lib_FKLangue'), editorClass: 'form-control',options:languages},
-          TLib_Name:      {type:'Text', title: $.i18n.t('topicLibelle_field.lib_name'), editorClass: 'form-control'},
+          TLib_FK_TLan_ID:      {type: 'Select', title: $.i18n.t('topicLibelle_field.lib_FKLangue'), editorClass: 'form-control',options:languages, validators: ['required']},
+          TLib_Name:      {type:'Text', title: $.i18n.t('topicLibelle_field.lib_name'), editorClass: 'form-control', validators: ['required']},
           TLib_FullPath:      {type:'Text', title: $.i18n.t('topicLibelle_field.lib_fullPath'), editorClass: 'form-control'},
           TLib_Definition:       {type:'TextArea', title: $.i18n.t('topicLibelle_field.lib_Definition'), editorClass: 'form-control'},
           TLib_FK_TTop_ID:      {type: 'Hidden', editorAttrs: {disabled: true}, title: $.i18n.t('topicLibelle_field.lib_FKTopic'), editorClass: 'form-control'},
